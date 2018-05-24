e8508b7128c41f732cdc16172cd14a79f5c414da
Amir Khazaie <733amir@gmail.com>
2018-05-24 21:29:48 +0430

    Add proper comment for exported errors

--- errors.go
+++ errors.go
@@ -2,8 +2,10 @@ package jalaali
 
 import "fmt"
 
+// ErrorNilReference is happening when a pointer is nil.
 type ErrorNilReference struct{}
 
+// ErrorInvalidYear is happening when year passed is is in proper range.
 type ErrorInvalidYear struct {
 	year int
 }
